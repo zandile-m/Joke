cb1e55583cd9c8f111498f6d4bdb634c9f1a8216
zandile-m <zandilemakhapela@gmail.com>
2025-11-13 19:06:35 +0200

    Updated the index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-function generateAns(response){
-    console.log(response);
+function showJoke(response) {
+  console.log(response.data.answer);
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.innerHTML = response.data.answer;
 }
 
-let apiKey= "944a6721ao9b6c1c3a743389fb2tdec0" ; 
-let context = "Make sure to provide a clear and precise answer " ;
-let prompt = "Tell me a funny joke" ;
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+function generateJoke() {
+  let apiKey = "944a6721a09b6c1c3a743389fb2tdec0";
+  let context = "Make sure to provide a clear and funny joke.";
+  let prompt = "Tell me a funny joke";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-axios.get(apiUrl).then(generateAns);
+  let button = document.querySelector("#joker");
+  button.innerHTML = "Loading...";
+
+  axios.get(apiUrl).then(function (response) {
+    showJoke(response);
+    button.innerHTML = "Tell me joke";
+  });
+}
+
+let jokeButton = document.querySelector("#joker");
+jokeButton.addEventListener("click", generateJoke);
