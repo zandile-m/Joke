d431dd447013e0cba4d468edd40c06a937f491eb
zandile-m <zandilemakhapela@gmail.com>
2025-11-14 02:58:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function showJoke(response) {
 
 function generateJoke() {
   let apiKey = "944a6721ao9b6c1c3a743389fb2tdec0";
-  let context = "Make sure to provide a clear and funny joke.";
+  let context = "Make sure to provide a clear and funny joke about everything.";
   let prompt = "Tell me a funny joke";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
